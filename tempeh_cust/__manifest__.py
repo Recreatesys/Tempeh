b851f4a72dd58c3421818ed27e354fe07161de58
Lau Siu Hin Marco <marcolau733@gmail.com>
2025-12-08 12:16:30 +0800

    [ADD] Added sale order report and num2chinese function for sale order print layout

--- tempeh_cust/__manifest__.py
+++ tempeh_cust/__manifest__.py
@@ -9,15 +9,15 @@
     """,
     'author': 'Lau Siu Hin',
     'website': '',
-    'depends': ['contacts', 'base', 'web', 'account'],
+    'depends': ['contacts', 'base', 'web', 'account', 'sale_management'],
     'data': [
         "views/invoice_inherit.xml",
         "report/report.xml",
+        "report/sale_order_report.xml",
         "views/product_template.xml",
         "views/res_company_view.xml",
         "views/account_move_view.xml",
     ],
-
 
     'installable': True,
     'application': False,
--- tempeh_cust/models/__init__.py
+++ tempeh_cust/models/__init__.py
@@ -1,3 +1,4 @@
 from . import product
 from . import res_company
 from . import account_move
+from . import sale_order
